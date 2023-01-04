22a1b84d332c4a6867f0931b7d6e7149d8ff27d6
miZyind <mizyind@gmail.com>
2023-01-04 10:37:54 +0700

    init: 7D2D-Mods project

---
+++ TPView/Plugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("miZyind.TPView", "TPView", "2023.01.04")]
+class TPView : BaseUnityPlugin
+{
+    void Awake()
+    {
+        Harmony.CreateAndPatchAll(typeof(TPView));
+    }
+}
